a56a9ee80d83ba1f483ea7f99a79249da2e0c02b
AmeliaMatthiesen <143598707+AmeliaMatthiesen@users.noreply.github.com>
2025-03-11 19:59:02 +0100

    #2 Initialize client and server structure with React, Vite, and Tailwind CSS

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Hello world</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
