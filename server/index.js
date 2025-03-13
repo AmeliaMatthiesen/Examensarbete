0fc8a15fef0575b45f190b272cfb207874b25967
AmeliaMatthiesen <143598707+AmeliaMatthiesen@users.noreply.github.com>
2025-03-13 20:51:02 +0100

    Initialise backend #3 Add dotenv configuration and MongoDB connection to server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json()); // Viktigt för att hantera JSON-data!
+app.use(express.json());
 app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+  })
+  .then(() => console.log(" MongoDB connected"))
+  .catch((err) => console.error(" MongoDB connection error:", err));
 
 // Test-route
 app.get("/api/test", (req, res) => {
