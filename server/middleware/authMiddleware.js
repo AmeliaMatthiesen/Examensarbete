2948471a203462b572ba07d2b56c49ccac153673
AmeliaMatthiesen <143598707+AmeliaMatthiesen@users.noreply.github.com>
2025-04-06 17:12:07 +0200

    Add middleware for protected routes #80 Implement JWT authentication middleware for user authorization

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,6 +1,27 @@
-const mockAuth = (req, res, next) => {
-  req.user = { _id: '65f05c5fe65b3f4b13b0c2aa' };
-  next();
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = (req, res, next) => {
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    const error = new Error ("Not authorized, no token");
+    error.statusCode = 401;
+    return next(error);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoder = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoder;
+    next();
+  } catch (error) {
+    const err = new Error("Not authorized, token failed");
+    error.statusCode = 401;
+    return next(err);
+  }
 };
 
-export default mockAuth;
+export default authMiddleware;
+
